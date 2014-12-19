7a0960f389d6e33e6fb71a2cdc36f8c761625507
Tomas Brambora <tomas.brambora@gmail.com>
2014-12-19 15:26:05 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject slack-nag "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Eat your vegetables, do your code reviews!"
+  :url "https://github.com/realyze/rb-slack-nagger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins  [
